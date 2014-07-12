9435df49b9875fd018d496104e5ccda3202c70ea
Sudavar <Sudavar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-12 21:04:34 +0000

    fixed wrong folder name
    git-svn-id: https://plugins.svn.wordpress.org/codescar-radio-widget/trunk@947559 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- radio-widget-settings.php
+++ radio-widget-settings.php
@@ -17,92 +17,92 @@ class Codescar_Radio_Widget_Settings {
     }
 
     public function cdscr_options_page() {
-        $settings_url = admin_url( 'options-general.php' ) . '?page=codescar-radio-widget';
+		$settings_url = admin_url( 'options-general.php' ) . '?page=codescar-radio-widget';
 
-        if ( ! current_user_can( 'manage_options' ) ) {
-            wp_die( __( 'You do not have sufficient permissions to access this page.', 'codescar-radio-widget' ) );
-        }
+    	if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( __( 'You do not have sufficient permissions to access this page.', 'codescar-radio-widget' ) );
+		}
 
-        echo '<div class="wrap">'
+		echo '<div class="wrap">'
              .'<h2><div id="icon-options-general" class="icon32"></div>'
              .__( 'Codescar Radio Widget Stations', 'codescar-radio-widget' ).'</h2>';
         $radio = get_option( 'cdscr_radio_settings' );
         $radio = maybe_unserialize( $radio );
 
         if ( isset( $_POST[ 'cdscr_add_new' ] ) && !isset( $_POST[ 'station_id' ] ) ){
-            if ( !isset( $_POST[ 'station_name' ] ) || empty( $_POST[ 'station_name' ] )
+			if ( !isset( $_POST[ 'station_name' ] ) || empty( $_POST[ 'station_name' ] )
             || !isset( $_POST[ 'station_url' ] ) || empty( $_POST[ 'station_url' ] ) ) {
-                echo '<div id="message" class="error"><p>'. __( 'Please fill all the boxes!', 'codescar-radio-widget' ) .'</p></div>';
-            } else {
+				echo '<div id="message" class="error"><p>'. __( 'Please fill all the boxes!', 'codescar-radio-widget' ) .'</p></div>';
+			} else {
                 $tmp = array();
                 $tmp[ 'name' ] = $_POST[ 'station_name' ];
                 $tmp[ 'url' ] = $_POST[ 'station_url' ];
                 $radio[ 'stations' ][] = $tmp;
                 var_dump("boalfmoa");
                 update_option( 'cdscr_radio_settings', $radio );
-            }
-        }
+			}
+		}
         if ( isset( $_POST[ 'station_id' ] ) ) {
-            if ( !isset( $_POST[ 'station_name' ] ) || empty( $_POST[ 'station_name' ] ) 
+		    if ( !isset( $_POST[ 'station_name' ] ) || empty( $_POST[ 'station_name' ] ) 
                 || !isset( $_POST[ 'station_url' ] ) || empty( $_POST[ 'station_url' ] ) ) {
-                    echo '<div id="message" class="error"><p>'
+			        echo '<div id="message" class="error"><p>'
                             . __( 'Please fill all the boxes!', 'codescar-radio-widget' ) .'</p></div>';
-            } else {
-                $tmp = array();
+		    } else {
+			    $tmp = array();
                 $tmp[ 'name' ] = $_POST[ 'station_name' ];
                 $tmp[ 'url' ] = $_POST[ 'station_url' ];
                 $rm_id = $_POST[ 'station_id' ];
                 $radio[ 'stations' ][ $rm_id ] = $tmp;
                 update_option( 'cdscr_radio_settings', $radio );
-            }
+		    }
         }
         if ( isset( $_GET[ 'action' ] ) ) {
-            if ( !isset( $_GET[ 'id' ] ) || empty( $_GET[ 'id' ] ) ) {
-                echo '<div id="message" class="error"><p>'. __( 'There was an error!', 'codescar-radio-widget' ) .'</p></div>';
-            } elseif ( $_GET[ 'action' ] == "delete" ) {
+			if ( !isset( $_GET[ 'id' ] ) || empty( $_GET[ 'id' ] ) ) {
+				echo '<div id="message" class="error"><p>'. __( 'There was an error!', 'codescar-radio-widget' ) .'</p></div>';
+			} elseif ( $_GET[ 'action' ] == "delete" ) {
                 //delete the record
                 $rm_id = $_GET[ 'id' ] - 1;
-                unset( $radio[ 'stations' ][ $rm_id ] );
+				unset( $radio[ 'stations' ][ $rm_id ] );
                 $radio[ 'stations' ] = array_values( $radio[ 'stations' ] );
                 update_option( 'cdscr_radio_settings', $radio );
-            } elseif ( $_GET[ 'action' ] == "edit" ) {
+			} elseif ( $_GET[ 'action' ] == "edit" ) {
                 $rm_id = $_GET[ 'id' ] - 1;
-                $station_to_edit = $radio[ 'stations' ][ $rm_id ];
+				$station_to_edit = $radio[ 'stations' ][ $rm_id ];
                 $station_to_edit[ 'id' ] = $rm_id;
-            }
+			}
         }
-        echo '<table class="widefat">
-                <thead>
-                    <tr>
+		echo '<table class="widefat">
+				<thead>
+				    <tr>
                         <th>'. __( 'ID', 'codescar-radio-widget' ).'</th>
-                        <th>'. __( 'Name', 'codescar-radio-widget' ).'</th>
-                        <th>'. __( 'Url', 'codescar-radio-widget' ).'</th>
+				        <th>'. __( 'Name', 'codescar-radio-widget' ).'</th>
+				        <th>'. __( 'Url', 'codescar-radio-widget' ).'</th>
                         <th>'. __( 'Action', 'codescar-radio-widget' ).'</th>
-                    </tr>
-                </thead>
-                <tfoot>
-                    <tr>
+				    </tr>
+				</thead>
+				<tfoot>
+				    <tr>
                         <th>'. __( 'ID', 'codescar-radio-widget' ).'</th>
-                        <th>'. __( 'Name', 'codescar-radio-widget' ).'</th>
-                        <th>'. __( 'Url', 'codescar-radio-widget' ).'</th>
+				        <th>'. __( 'Name', 'codescar-radio-widget' ).'</th>
+				        <th>'. __( 'Url', 'codescar-radio-widget' ).'</th>
                         <th>'. __( 'Action', 'codescar-radio-widget' ).'</th>
-                    </tr>
-                </tfoot>
-                <tbody>';
+				    </tr>
+				</tfoot>
+				<tbody>';
         $id = 1;
-        foreach( $radio[ 'stations' ] as $station ){
-            echo '<tr>
+		foreach( $radio[ 'stations' ] as $station ){
+			echo '<tr>
                      <td>'. $id .'</td>
-                     <td>'. $station[ 'name' ] .'</td>
-                     <td><a class="edit_link" target="_blank" href="'. $station[ 'url' ] .'">'. $station[ 'url' ] .'</a></td>
-                     <td>
+				     <td>'. $station[ 'name' ] .'</td>
+				     <td><a class="edit_link" target="_blank" href="'. $station[ 'url' ] .'">'. $station[ 'url' ] .'</a></td>
+				     <td>
                         <a class="edit_link" href="'. $settings_url .'&action=edit&id='. $id .'">'
                             . __( 'Edit', 'codescar-radio-widget' ) .'</a> | 
                         <a class="delete_link" href="'. $settings_url .'&action=delete&id='. $id .'">'
                             . __( 'Delete', 'codescar-radio-widget' ) .'</a></td>
-                </tr>';
+		   		</tr>';
             $id++;
-        }
+		}
         $flag = isset( $station_to_edit );
         echo '<tr><form method="POST" action="'. $settings_url .'">';
         if ($flag) {
@@ -123,5 +123,5 @@ class Codescar_Radio_Widget_Settings {
         echo '<input type="hidden" name="cdscr_add_new" value="1" /></td>
               </tbody></table></div>';
         echo '</div>';
-    }
+	}
 }
